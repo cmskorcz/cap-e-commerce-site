b2e91ce5594279f9cb887c0b928f1eb361199202
cmskorcz <chaseskorey@icloud.com>
2022-11-03 06:15:27 -0400

    added rows and cols and justified image to center

--- client/src/components/BookFigure.js
+++ client/src/components/BookFigure.js
@@ -1,14 +1,20 @@
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function BookFigure() {
   return (
     <Container>
-      <Image 
-        src='/img/placeholder.jpeg' 
-        fluid
-        className='my-3'
-      />
+      <Row className='justify-content-center'>
+        <Col xs={10}>
+          <Image 
+            src='/img/placeholder.jpeg' 
+            fluid
+            className='my-3'
+          />
+        </Col>
+      </Row>
     </Container>
   )
 }
