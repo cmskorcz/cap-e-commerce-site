db309187fbd5f902ab27d8e435bb8259958a2049
cmskorcz <chaseskorey@icloud.com>
2022-11-03 06:34:20 -0400

    added book name, author, and button link

--- client/src/components/BookFigure.js
+++ client/src/components/BookFigure.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button'
 
 function BookFigure() {
   return (
@@ -13,6 +14,21 @@ function BookFigure() {
             fluid
             className='my-3'
           />
+        </Col>
+        <Col xs={10} className='text-center'>
+          <h2>The EKG Guidebook</h2>
+          <p className='lead'>By: Mikel Capillari</p>
+        </Col>
+        <Col xs={10}>
+          <div className='d-grid gap-2'>
+            <Button 
+              href='#'
+              variant='primary'
+              size='lg'
+            >
+              Buy Now
+            </Button>
+          </div>
         </Col>
       </Row>
     </Container>
